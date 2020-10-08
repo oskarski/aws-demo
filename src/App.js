ec29306091680ff3aaf9a80cac3c560745ccf088
Oskar Kupski <oskar.kupski@gmail.com>
2020-10-08 14:17:10 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { API, Auth } from 'aws-amplify'
+import { listPosts } from './graphql/queries'
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+
 
 function App() {
+  const [posts, setPosts] = useState([])
+  useEffect(() => {
+    fetchPosts();
+    checkUser(); // new function call
+  }, []);
+  async function fetchPosts() {
+    try {
+      const postData = await API.graphql({ query: listPosts });
+      setPosts(postData.data.listPosts.items)
+    } catch (err) {
+      console.log({ err })
+    }
+  }
+
+  async function checkUser() {
+    const user = await Auth.currentAuthenticatedUser();
+    console.log('user: ', user);
+    console.log('user attributes: ', user.attributes);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div>
+        <h1>Hello World</h1>
+        {
+          posts.map(post => (
+              <div key={post.id}>
+                <h3>{post.name}</h3>
+                <p>{post.location}</p>
+              </div>
+          ))
+        }
+        <AmplifySignOut />
+      </div>
+  )
 }
 
-export default App;
+export default withAuthenticator(App);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Router from './Router';
 import * as serviceWorker from './serviceWorker';
+import Amplify from 'aws-amplify'
+import config from './aws-exports'
+
+Amplify.configure(config)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router />
   </React.StrictMode>,
   document.getElementById('root')
 );
